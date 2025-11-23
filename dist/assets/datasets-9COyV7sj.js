efcfa72fb79896cf4b4f3b7f996f1ca2943f76a0
erikmuller@t-online.de <erikmuller@t-online.de>
2025-11-23 11:27:28 +0100

    dataset subsection scaling fix

--- dist/assets/datasets-9COyV7sj.js
+++ dist/assets/datasets-9COyV7sj.js
@@ -1,4 +1,4 @@
-import{r as oO,e as aO}from"./layout-BlGWQYcK.js";/**
+import{r as oO,e as aO}from"./layout-Ca7QLL4J.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
