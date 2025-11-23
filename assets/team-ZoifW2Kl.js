b28f248616f98b4775690b1b7f37f9966a4a5e56
erikmuller@t-online.de <erikmuller@t-online.de>
2025-11-23 11:24:33 +0100

    Updates

--- assets/team-ZoifW2Kl.js
+++ assets/team-ZoifW2Kl.js
@@ -1,4 +1,4 @@
-import{r as n,g as e,e as o}from"./layout-g-r-J9VJ.js";const s=document.querySelector("#app");s.innerHTML=n("team",`
+import{r as n,g as e,e as o}from"./layout-Ca7QLL4J.js";const s=document.querySelector("#app");s.innerHTML=n("team",`
   <section class="page-header">
     <div class="container">
       <div class="title-row">
